0ac76b2e3ea6c60ca674601e025437d12e6eb428
jacqui-maclean <inaripaddler@gmail.com>
2023-07-08 12:55:42 +0100

    Handle games with noo image

--- src/services/crop-image.ts
+++ src/services/crop-image.ts
@@ -1,5 +1,6 @@
+import Placeholder from "../assets/no-image-placeholder.webp";
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return Placeholder;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
